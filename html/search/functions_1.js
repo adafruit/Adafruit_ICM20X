3aa890cce27ff659974badbb1075b63ae3443eac
Doxygen CI <travis@travis-ci.org>
2020-02-29 00:52:54 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/functions_1.js
+++ html/search/functions_1.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['adafruit_5ficm20649',['Adafruit_ICM20649',['../class_adafruit___i_c_m20649.html#a06fd2feafa02047f38f029bb454b04ce',1,'Adafruit_ICM20649']]],
+  ['adafruit_5ficm20948',['Adafruit_ICM20948',['../class_adafruit___i_c_m20948.html#a549731b2539710ac4b81472807835220',1,'Adafruit_ICM20948']]],
   ['adafruit_5ficm20x',['Adafruit_ICM20X',['../class_adafruit___i_c_m20_x.html#ad10aa12a5bd5391be935cdc2bfeed5fb',1,'Adafruit_ICM20X']]],
   ['adafruit_5ficm20x_5faccelerometer',['Adafruit_ICM20X_Accelerometer',['../class_adafruit___i_c_m20_x___accelerometer.html#ace3e82c10b3ed60b0fce8beef354c89d',1,'Adafruit_ICM20X_Accelerometer']]],
   ['adafruit_5ficm20x_5fgyro',['Adafruit_ICM20X_Gyro',['../class_adafruit___i_c_m20_x___gyro.html#a8679eac1509ba4fb8a45e364c29d403d',1,'Adafruit_ICM20X_Gyro']]],
